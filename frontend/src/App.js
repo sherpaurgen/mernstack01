14860ab7f596514f8d3d00f3e09d7f2dba2ae02b
urgen sherpa <7010051+sherpaurgen@users.noreply.github.com>
2020-11-08 22:38:30 +0545

    ui parts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
 
-export default const App = () => {
+
+const App = () => {
   return (
-    <div>
-      hello world
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
+
+export default App
